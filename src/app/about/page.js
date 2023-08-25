88e8a0e6259a344ca10e637070a8c9abc02a9b67
ZuhebAkhter <zuheb1998@gmail.com>
2023-08-25 18:25:12 +0530

    fisrtatsk

---
+++ src/app/about/page.js
@@ -0,0 +1,9 @@
+const News=()=>{
+    return(
+        <h1>
+            Thus is about page
+        </h1>
+    )
+}
+
+export default  News
---
+++ src/app/news/page.js
@@ -0,0 +1,11 @@
+
+
+const News=()=>{
+    return(
+        <h1>
+            Thus is NewsPage
+        </h1>
+    )
+}
+
+export default  News
